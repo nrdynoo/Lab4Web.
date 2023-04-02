5d01cd75cdc3b51cdf6263de3b00f3e5dca87b6f
nrdynoo <115916423+nrdynoo@users.noreply.github.com>
2023-04-02 11:01:13 +0700

    PHP Modular
    membuat folder:
    1. header.php
    2. footer.php
    3. home.php
    4. about.php
    5. kontak.php
    6. index.php

---
+++ lab4_php_modular/kontak.php
@@ -0,0 +1,7 @@
+<?php require('header.php'); 
+?> <div class="kontak">
+    <h2>Ini Halaman kontak</h2>
+    <p>Ini adalah bagian content dari halaman.</p>
+ </div>
+ 
+ <?php require('footer.php'); ?>
